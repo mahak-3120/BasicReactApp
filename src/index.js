89ddad53a7669f3835cf636d315df04a0d0ab0f8
mahak-3120 <sainimahak9@gmail.com>
2021-02-03 21:13:38 +0530

    made cards of robots

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CardList />
   </React.StrictMode>,
   document.getElementById('root')
 );
