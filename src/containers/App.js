2e79b4bbf3de0b2236e4eed7032fd368bf2769c8
mahak-3120 <sainimahak9@gmail.com>
2021-02-03 22:51:10 +0530

    complete functioning app

---
+++ src/containers/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll.js'
+import './App.css';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            searchfield : ''
+        }
+    }
+// lifecycle method : it calls render whenever the state changes
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value});
+    }
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        });
+        if (robots.length === 0) {
+            return <h1 className="tc">Loading</h1>
+        } else
+        {
+        return (
+            <div className="tc">
+                <h1 className="f2">ROBOFRIENDS</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
+            </div>
+        )
+        }
+    }
+}
+
+export default App;
